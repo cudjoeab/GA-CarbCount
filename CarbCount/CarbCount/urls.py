cb2149080900d463bf56adb1c8d4f84242e98613
adam-weiler <adam.weiler@yahoo.ca>
2019-09-09 14:03:29 -0400

    Adding urls.py. Python will now redirect to React. Admin section added.

--- CarbCount/CarbCount/urls.py
+++ CarbCount/CarbCount/urls.py
@@ -1,4 +1,4 @@
-"""CarbCount URL Configuration
+"""myapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path  # Do we need to import path..?
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # ... the rest of the urlpatterns ...
+    # must be catch-all for pushState to work
+    path('admin', admin.site.urls),
+    url(r'^', views.FrontendAppView.as_view())
 ]
