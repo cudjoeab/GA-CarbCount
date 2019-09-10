485ac83b2276de9116ac58ab7309d2c3169580c6
cudjoeab <cudjoeab@gmail.com>
2019-09-10 12:05:41 -0400

    edits to models.py

---
+++ CarbCount/CarbCount/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Practitioner)
+admin.site.register(Diabetic)
+admin.site.register(Recipe)
+admin.site.register(Meal)
+admin.site.register(Log)
--- CarbCount/CarbCount/models.py
+++ CarbCount/CarbCount/models.py
@@ -1,55 +1,55 @@
-
 from django.db import models
-from django.contrb.auth.models import User
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
-from django.core.validators import 
-    MinValueValidator,
-    MaxValueValidator
+from django.core.validators import ( MinLengthValidator, MaxLengthValidator,MinValueValidator, MaxValueValidator,  )
 
 
-class User(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    morning_ratio = models.FloatField()
-    afternoon_ratio = models.FloatField()
-    evening_ratio = models.FloatField()
-    night_ratio = models.FloatField()
-    moring_cor_factor = models.FloatField()
-    afternoon_cor_factor = models.FloatField()
-    evening_cor_factor = models.FloatField()
-    night_cor_factor = models.FloatField()
-    moring_target = models.FloatField()
-    afternoon_target = models.FloatField()
-    evening_target = models.FloatField()
-    night_target = models.FloatField()
-    insulin_duration = models.FloatField()
-    insulin_type = CharField(max_length=255)
-    practcioner_id = models.ForeignKey(Practioner, models.on_delete=models.CASCADE, default=None, related_name="practioner")
-
-    def __str__(self):
-        return f'User: {self.first_name} - {self.last_name} - {self.email}'
-
-class Practioner(models.Model):
+class Practitioner(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     clinic_name = models.CharField(max_length=255)
-    license = models.IntegerField()
+    license_id = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
 
     def __str__(self):
-        return f'Practioner: {self.first_name} - {self.last_name}'
+        return f'Practitioner: {self.first_name} - {self.last_name}'
+
+class Diabetic(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    morning_ratio = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    afternoon_ratio = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    evening_ratio = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    night_ratio = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    moring_cor_factor = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    afternoon_cor_factor = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    evening_cor_factor = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    night_cor_factor = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    moring_target = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    afternoon_target = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    evening_target = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    night_target = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    insulin_duration = models.FloatField()
+    insulin_type = models.CharField(max_length=255)
+    practitioner_id = models.ForeignKey(Practitioner, on_delete=models.CASCADE, null=True, blank=True, related_name="practitioner")
+
+    def __str__(self):
+        return f'Diabetic: {self.first_name} - {self.last_name} - {self.email}'
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
-    total_carbs = models.IntegerField(validators=[MinValueValidator(1)], null=True)
-    total_fibre = models.IntegerField(validators=[MinValueValidator(1)], null=True)
-    ingredients = models.IntegerField()
-    total_servings = model.IntegerField()
-    description = models.IntegerField()
+    total_carbs = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    total_fibre = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    ingredients = ArrayField(models.CharField(max_length=255, blank=True))
+    total_servings = models.IntegerField(validators=[MinValueValidator(1)])
+    description = models.TextField(
+        validators=[MinLengthValidator(10), MaxLengthValidator(500)]
+    )
     tags = models.CharField(max_length=255)
     image_link = models.CharField(max_length=255)
-    user_id = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="users"
+    diabetic_id = models.ForeignKey(
+        Diabetic, on_delete=models.CASCADE, related_name="Diabetics"
     )
     
     def __str__(self):
@@ -57,27 +57,27 @@ class Recipe(models.Model):
 
 class Meal(models.Model): 
     date = models.DateField()
-    food = [] 
-    carbs = models.FloatField()
-    fibre = models.FloatField()
-    meal_type = models.CharField()
-    blood_glucose = models.FloatField()
-    insulin_dose = models.FloatField() 
+    food = ArrayField(models.CharField(max_length=255, blank=True)) 
+    carbs = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    fibre = models.FloatField(validators=[MinValueValidator(0)], null=False)
+    meal_type = models.CharField(max_length=255)
+    blood_glucose = models.FloatField(validators=[MinValueValidator(0)], null=True)
+    insulin_dose = models.FloatField(validators=[MinValueValidator(0)], null=False) 
     
     def __str__(self):
         return f'''
         date: {self.date} \n
         food: {self.food} \n
-        total carbs: {self.carb} grams of carb \n
+        total carbs: {self.carbs} grams of carb \n
         meal_type: {self.meal_type} \n
         blood_glucose: Your blood sugar is {self.blood_glucose} mmol/L \n
-        insulin_dose: Please take {self.insulin_dose} units of {User.insulin_type}\n 
+        insulin_dose: Please take {self.insulin_dose} units of {Diabetic.insulin_type}\n 
         '''    
 
 class Log(models.Model):
     meal_id = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name ="meal")
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    diabetic_id = models.ForeignKey(Diabetic, on_delete=models.CASCADE, related_name="diabetic")
 
     def __str__(self):
-        return f'Log: {self.meal_id} - {self.user_id}'
+        return f'Log: {self.meal_id} - {self.diabetic_id}'
     
