1b23aac185d7588d51da9be72dd6e3d4a9418454
Mark Jarvis <mtgjarvis@gmail.com>
2019-09-11 10:56:59 -0400

    installed djangoFramework

--- CarbCount/CarbCount/settings.py
+++ CarbCount/CarbCount/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CarbCount'
+    'CarbCount',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
