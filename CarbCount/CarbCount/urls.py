2400735f585ed98841b0927109826e3d95c53ecb
adam-weiler <adam.weiler@yahoo.ca>
2019-09-11 15:57:21 -0400

    Updating the urls paths to point to each individual table.

--- CarbCount/CarbCount/urls.py
+++ CarbCount/CarbCount/urls.py
@@ -22,11 +22,11 @@ from .api import PractitionerViewSet, DiabeticViewSet, MealViewSet, RecipeViewSe
 
 
 router = routers.DefaultRouter()  # Imports the class DefaultRouter from routers.
-router.register('api/carbcount', PractitionerViewSet, 'carbcount') # Registers the router; includes all CRUD operations.
-router.register('api/carbcount', DiabeticViewSet, 'carbcount')
-router.register('api/carbcount', MealViewSet, 'carbcount')
-router.register('api/carbcount', RecipeViewSet, 'carbcount')
-router.register('api/carbcount', LogViewSet, 'carbcount')
+router.register('api/practitioner', PractitionerViewSet, 'practitioner') # Registers the router; includes all CRUD operations.
+router.register('api/diabetic', DiabeticViewSet, 'diabetic')
+router.register('api/meal', MealViewSet, 'meal')
+router.register('api/recipe', RecipeViewSet, 'recipe')
+router.register('api/log', LogViewSet, 'log')
 
 # Only use the registered url endpoints; no longer need old-timey Django routes.
 urlpatterns = [
