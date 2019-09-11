52d89a9f4d1fa3a669cbc86e9cc614e28f4adf4d
adam-weiler <adam.weiler@yahoo.ca>
2019-09-11 19:12:33 -0400

    API routes are now working. I needed to add something in settings.py for Ubuntu, please check if it affects your machine. Reverting Authentication changes made to App.js.

--- CarbCount/frontend/src/App.js
+++ CarbCount/frontend/src/App.js
@@ -1,29 +1,26 @@
-import AuthService from './components/AuthService';
-import withAuth from './components/withAuth';
-const Auth = new AuthService();
-
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  handleLogout(){
-    Auth.logout()
-    this.props.history.replace('/login');
- }
-
-  return(
+  return (
     <div className="App">
-        <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome {this.props.user.username}</h2>
-        </div>
-        <p className="App-intro">
-            <button type="button" className="form-submit" onClick={this.handleLogout.bind(this)}>Logout</button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
-export default withAuth(App);  // Is this right???
