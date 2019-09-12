0a7a5da288d9d6d1fdf8e74ef7c49b39d6e72683
cudjoeab <cudjoeab@gmail.com>
2019-09-12 11:39:42 -0400

    removed the commented  method from views.py, created a new file: calculations.py

---
+++ CarbCount/CarbCount/calculations.py
@@ -0,0 +1,5 @@
+ """" 
+ The instance methods here are to take care of any calculations required
+ """
+
+ 
--- CarbCount/CarbCount/views.py
+++ CarbCount/CarbCount/views.py
@@ -25,8 +25,6 @@ class FrontendAppView(View):
                 """,
                 status=501,
             )
+
+
     
-    # def signup(request):
-    #     form = UserCreationForm()
-    #     context = { 'form': form }
-    #     response = render(request, '') 
