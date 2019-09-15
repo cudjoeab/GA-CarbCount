51171250fdd2f15a802fdcf3f3a8f4a5741f66d8
cudjoeab <cudjoeab@gmail.com>
2019-09-15 13:15:21 -0400

    import models that we have defined. This is what we are running tests on

--- CarbCount/CarbCount/tests.py
+++ CarbCount/CarbCount/tests.py
@@ -2,5 +2,9 @@
 import datetime
 from decimal import Decimal 
 
+# import test library 
 from django.test import TestCase
- 
+
+# import models that are needed for testing 
+from . import models
+from .models import Meal 
