b612ded1623fcc7061a520e4c22d67e9e39bfe0c
cudjoeab <cudjoeab@gmail.com>
2019-09-15 12:16:54 -0400

    created a DJANGO test file: tests.py. To run tests use 'python manage.py test' in the terminal

--- CarbCount/CarbCount/test_calculations.py
+++ CarbCount/CarbCount/test_calculations.py
@@ -1,2 +1,16 @@
-import calculations
+
 import unittest 
+
+class TestMealMethods(unittest.TestCase): 
+
+    def test_calculate_net_carb(self):
+        pass
+
+    def test_calculate_net_fibre(self):
+        pass
+
+    def total_carb(self):
+        pass 
+
+if __name__ == '__main__':
+    unittest.main() 
