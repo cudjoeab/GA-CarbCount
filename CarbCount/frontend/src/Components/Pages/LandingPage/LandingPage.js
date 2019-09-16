807e3ef1ae47057ac8146c768bc241d7ca560d91
cudjoeab <cudjoeab@gmail.com>
2019-09-16 16:03:31 -0400

    edited photos for visibility

--- CarbCount/frontend/src/Components/Pages/LandingPage/LandingPage.js
+++ CarbCount/frontend/src/Components/Pages/LandingPage/LandingPage.js
@@ -13,9 +13,9 @@ import './LandingPage.css';
 // imports for the background slideshow 
 import BackgroundSlideshow from 'react-background-slideshow'
 
-import image1 from './Images/burger.jpg'
-import image2 from './Images/eastermeal.jpg'
-import image3 from './Images/noodles.jpg'
+import image1 from './Images/1.png'
+import image2 from './Images/2.png'
+import image3 from './Images/3.png'
 
 
 class LandingPage extends Component {
@@ -31,11 +31,11 @@ class LandingPage extends Component {
             <section className='landingPageInfo'>
                 <h2>Carb Counting Made Easy (LandingPage.js)</h2>
                 <p>Description</p>
-                <h3>Track over x number foods</h3>
+                <h3> Carbohydrate info for numerous foods</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in nunc sed nisl fringilla venenatis.</p>
-                <h3>Delicious recipes</h3>
+                <h3>Rapid Insulin Calculator</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in nunc sed nisl fringilla venenatis.</p>
-                <h3>Third thing</h3>
+                <h3>Keep track of meal history</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in nunc sed nisl fringilla venenatis.</p>
                 
             </section>
