f662a7197f63ac29ae1a65613ede0c1781107e6e
cudjoeab <cudjoeab@gmail.com>
2019-09-18 17:48:07 -0400

    formatting for food log and saved recipes

--- CarbCount/frontend/src/Components/Pages/SavedRecipes/SavedRecipes.js
+++ CarbCount/frontend/src/Components/Pages/SavedRecipes/SavedRecipes.js
@@ -13,12 +13,12 @@ import './SavedRecipes.css';
 
 class SavedRecipes extends Component {
     JSONresponse = [
-        { name: 'Pizza', total_carbs: 26.08, total_fibre: 1.6, meal_type: 'Dinner', blood_glucose: 7.6, insulin_dose: 5  }, 
-        { name: 'Spaghetti', total_carbs: 26.81, total_fibre: 1.1, meal_type: 'Dinner', blood_glucose: 11.7, insulin_dose: 10.79 }, 
-        { name: 'Grilled Chicken', total_carbs: 27.4, total_fibre: 1.7, meal_type: 'Breakfast', blood_glucose: 9.0, insulin_dose: 3 }, 
-        { name: 'Lasagna', total_carbs: 53.02, total_fibre: 2.3, meal_type: 'Lunch', blood_glucose: 16, insulin_dose: 8 }, 
-        { name: 'Stir-Fry', total_carbs: 19.06, total_fibre: 3.3, meal_type: 'Snack', blood_glucose: 13, insulin_dose: 1 }, 
-        { name: 'Curry', total_carbs: 15.39, total_fibre: 3.1, meal_type: 'Snack', blood_glucose: 12, insulin_dose: 1 }
+        { name: 'Pizza', total_carbs: 26.08, total_fibre: 1.6, ingredients: 'Dinner', total_servings: 7.6, description: 5, total_servings: 7.6, description: 5  }, 
+        { name: 'Spaghetti', total_carbs: 26.81, total_fibre: 1.1, ingredients: 'Dinner', total_servings: 11.7, description: 10.79, total_servings: 7.6, description: 5 }, 
+        { name: 'Grilled Chicken', total_carbs: 27.4, total_fibre: 1.7, ingredients: 'Breakfast', total_servings: 9.0, description: 3, total_servings: 7.6, description: 5 }, 
+        { name: 'Lasagna', total_carbs: 53.02, total_fibre: 2.3, ingredients: 'Lunch', total_servings: 16, description: 8, total_servings: 7.6, description: 5 }, 
+        { name: 'Stir-Fry', total_carbs: 19.06, total_fibre: 3.3, ingredients: 'Snack', total_servings: 13, description: 1, total_servings: 7.6, description: 5 }, 
+        { name: 'Curry', total_carbs: 15.39, total_fibre: 3.1, ingredients: 'Snack', total_servings: 12, description: 1, total_servings: 7.6, description: 5 }
     ];
 
     // name
