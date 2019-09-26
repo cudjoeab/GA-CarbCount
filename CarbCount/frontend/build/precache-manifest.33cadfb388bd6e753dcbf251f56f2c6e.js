dc2bb23ff75634ea3eaa74ca2f399d522c60470c
adam-weiler <adam.weiler@yahoo.ca>
2019-09-26 10:24:56 -0400

    Adding static-root to settings file.

---
+++ CarbCount/frontend/build/precache-manifest.33cadfb388bd6e753dcbf251f56f2c6e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39739f5d8fcf4b2e578488c1fdc6887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b5b54af86cdcd34cbce",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9e5ce978e16c0938065d",
+    "url": "/static/css/main.4320afe4.chunk.css"
+  },
+  {
+    "revision": "2b5b54af86cdcd34cbce",
+    "url": "/static/js/2.76ff960a.chunk.js"
+  },
+  {
+    "revision": "9e5ce978e16c0938065d",
+    "url": "/static/js/main.e8fe6710.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  },
+  {
+    "revision": "726ae638423778454d89c7651ca32ab4",
+    "url": "/static/media/1.726ae638.png"
+  },
+  {
+    "revision": "0c66fc334697610fd6ea71a5dac96ac5",
+    "url": "/static/media/2.0c66fc33.png"
+  },
+  {
+    "revision": "36103122d7c3e80ba3de9e03e889324d",
+    "url": "/static/media/3.36103122.png"
+  },
+  {
+    "revision": "76d0cd08ac2b2c132d099c30594e9be0",
+    "url": "/static/media/abigail_cudjoe.76d0cd08.jpeg"
+  },
+  {
+    "revision": "3943b0c19155fe89918e695d6d753cf1",
+    "url": "/static/media/adam_weiler.3943b0c1.jpeg"
+  },
+  {
+    "revision": "d642f6815dd170d6f057ee02c350e665",
+    "url": "/static/media/larry_medina.d642f681.jpeg"
+  },
+  {
+    "revision": "21ee013770fe9ea287318098f8e07f8a",
+    "url": "/static/media/logo-03.21ee0137.png"
+  },
+  {
+    "revision": "e043cb65f6b1bf243f65d76cd75d840d",
+    "url": "/static/media/mark_jarvis.e043cb65.jpeg"
+  },
+  {
+    "revision": "00c45066119f44ba8c606feed7bc9568",
+    "url": "/static/media/osemke_nwaak.00c45066.jpeg"
+  },
+  {
+    "revision": "1bb5d2dd72da457d4afb8c9085025b5e",
+    "url": "/static/media/powered_by_fatsecret.1bb5d2dd.svg"
+  }
+]);
