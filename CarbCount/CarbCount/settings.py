31aca108c4cfdce7ce0a14fe863c35b1e052e373
adam-weiler <adam.weiler@yahoo.ca>
2019-10-17 11:34:31 -0400

    Disabling whitenoise.

--- CarbCount/CarbCount/settings.py
+++ CarbCount/CarbCount/settings.py
@@ -21,7 +21,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Adding this - Adam
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
